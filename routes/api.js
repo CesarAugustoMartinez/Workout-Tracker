5d9ff12ce1a605de2dccde3a2c39c3988dd9a76c
Cesar Martinez <cesaguma@hotmail.com>
2020-12-17 02:14:57 -0500

    Creating a file called api.js into routes folder to handle api routes

---
+++ routes/api.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+const Exercise = require("../models/exercise.js");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  router.put("/api/workouts/:id", (req, res) => {
+    Exercise.create(req.body)
+      .then(({ _id }) => Workout.findOneAndUpdate({_id:req.params.id}, { $push: { exercises: _id } }, { new: true }))
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+// router.get("/api/workouts/range", (req, res) => {
+//     Workout.find({})
+//       .sort({ date: -1 })
+//       .then(dbWorkout => {
+//         res.json(dbWorkout);
+//       })
+//       .catch(err => {
+//         res.status(400).json(err);
+//       });
+//   });
+
+
+
+module.exports = router;
